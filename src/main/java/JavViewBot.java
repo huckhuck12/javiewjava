d35fef16dfdf3de2cbeb1317617bdb4bb28f4500
huck12138 <huck12138@gmail.com>
2022-02-14 17:38:33 +0800

    test

--- src/main/java/JavViewBot.java
+++ src/main/java/JavViewBot.java
@@ -1,22 +1,41 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ForceReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.InvalidObjectException;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class JavViewBot extends TelegramLongPollingBot {
+    private static final String LOGTAG = "CHANNELHANDLERS";
+
+    private static final int WAITINGCHANNEL = 1;
+
+    private static final String HELP_TEXT = "Send me the channel username where you added me as admin.";
+    private static final String CANCEL_COMMAND = "/stop";
+    private static final String AFTER_CHANNEL_TEXT = "A message to provided channel will be sent if the bot was added to it as admin.";
+    private static final String WRONG_CHANNEL_TEXT = "Wrong username, please remember to add *@* before the username and send only the username.";
+    private static final String CHANNEL_MESSAGE_TEXT = "This message was sent by *@updateschannelbot*. Enjoy!";
+    private static final String ERROR_MESSAGE_TEXT = "There was an error sending the message to channel *%s*, the error was: ```%s```";
+
+    private final ConcurrentHashMap<Long, Integer> userState = new ConcurrentHashMap<>();
+
     @Override
     public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
-
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+        try {
+            Message message = update.getMessage();
+            if (message != null && message.hasText()) {
+                try {
+                    handleIncomingMessage(message);
+                } catch (InvalidObjectException e) {
+                    System.out.println(e);
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
@@ -28,5 +47,110 @@ public class JavViewBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "5114265387:AAH6oXXEvS0DDK5pMGOiIILLhfX-UXpmlIs";
+    }
+
+    private void handleIncomingMessage(Message message) throws InvalidObjectException {
+        int state = userState.getOrDefault(message.getFrom().getId(), 0);
+        switch(state) {
+            case WAITINGCHANNEL:
+                onWaitingChannelMessage(message);
+                break;
+            default:
+                sendHelpMessage(message.getChatId(), message.getMessageId(), null);
+                userState.put(message.getFrom().getId(), WAITINGCHANNEL);
+                break;
+        }
+    }
+
+    private void onWaitingChannelMessage(Message message) throws InvalidObjectException {
+        try {
+            if (message.getText().equals(CANCEL_COMMAND)) {
+                userState.remove(message.getFrom().getId());
+                sendHelpMessage(message.getChatId(), message.getMessageId(), null);
+            } else {
+                if (message.getText().startsWith("@") && !message.getText().trim().contains(" ")) {
+                    execute(getMessageToChannelSent(message));
+                    sendMessageToChannel(message.getText(), message);
+                    userState.remove(message.getFrom().getId());
+                } else {
+                    execute(getWrongUsernameMessage(message));
+                }
+            }
+        } catch (TelegramApiException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void sendMessageToChannel(String username, Message message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(username.trim());
+
+        sendMessage.setText(CHANNEL_MESSAGE_TEXT);
+        sendMessage.enableMarkdown(true);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            sendErrorMessage(message, e.getMessage());
+        }
+    }
+
+    private void sendErrorMessage(Message message, String errorText) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(String.valueOf(message.getChatId()));
+        sendMessage.setReplyToMessageId(message.getMessageId());
+
+        sendMessage.setText(String.format(ERROR_MESSAGE_TEXT, message.getText().trim(), errorText.replace("\"", "\\\"")));
+        sendMessage.enableMarkdown(true);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static SendMessage getWrongUsernameMessage(Message message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(String.valueOf(message.getChatId()));
+        sendMessage.setReplyToMessageId(message.getMessageId());
+
+        ForceReplyKeyboard forceReplyKeyboard = new ForceReplyKeyboard();
+        forceReplyKeyboard.setSelective(true);
+        sendMessage.setReplyMarkup(forceReplyKeyboard);
+
+        sendMessage.setText(WRONG_CHANNEL_TEXT);
+        sendMessage.enableMarkdown(true);
+        return sendMessage;
+    }
+
+    private static SendMessage getMessageToChannelSent(Message message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(String.valueOf(message.getChatId()));
+        sendMessage.setReplyToMessageId(message.getMessageId());
+
+        sendMessage.setText(AFTER_CHANNEL_TEXT);
+        return sendMessage;
+    }
+
+    private void sendHelpMessage(Long chatId, Integer messageId, ReplyKeyboardMarkup replyKeyboardMarkup) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setReplyToMessageId(messageId);
+        if (replyKeyboardMarkup != null) {
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        }
+
+        sendMessage.setText(HELP_TEXT);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e);
+        }
     }
 }
